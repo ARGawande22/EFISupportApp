e94cec3e37e0169a039f506e0c92a56826e4f445
Akash R Gawande <akashrgawande@gmail.com>
2026-07-18 20:52:51 +0530

    EFI-006 All minor fix completed

--- frmSupportApp.cs
+++ frmSupportApp.cs
@@ -90,8 +90,8 @@ namespace EFISupportApp
                 return;
             }
 
-            DataSet ds = ReadPayBillNPS14PerScheduledPDF.ExtractFromPdf(txtPath.Text);
-            DataSet ds1 = ReadPayBillNPS14PerScheduledPDFv1.ExtractFromPdf(txtPath.Text);
+            //DataSet ds = ReadPayBillNPS14PerScheduledPDF.ExtractFromPdf(txtPath.Text);
+            DataSet ds = ReadPayBillNPS14PerScheduledPDFv1.ExtractFromPdf(txtPath.Text);
         }
 
         private void btnNGRec_Click(object sender, EventArgs e)
